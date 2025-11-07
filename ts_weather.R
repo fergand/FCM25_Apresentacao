ae3d5256eb4f98abb0d39a1848deec4edc62b242
Fernando Andrade <ffandra@gmail.com>
2025-11-07 15:07:50 -0300

    add: analises ts inicias

---
+++ ts_weather.R
@@ -0,0 +1,94 @@
+library(tidyverse)
+library(fable)
+library(fabletools)
+library(ggthemes)
+set_theme(theme_clean())
+suppressWarnings(set_theme(theme_clean()))
+library(lubridate)
+library(feasts)
+library(mice)
+
+df <- read.csv("PalmerStation_Daily_Weather.csv", sep = ",", header = T) %>%
+  as_tibble()
+
+glimpse(df) # nota-se NAs em algumas variaveis
+
+df %>% 
+  select(Temperature.Average..C.) %>% 
+  summary()
+
+df %>% names()
+
+df %>% 
+  mutate(year = year(Date)) %>% 
+  group_by(year) %>% 
+  summarise(
+    total_dias_ano = n(),                             
+    dias_com_NA = sum(is.na(Temperature.Average..C.)),
+    ano_completo_em_NA = dias_com_NA == total_dias_ano
+  ) %>% 
+  print(n = 40) # Anos completos a partir de 1997
+
+# para uma serie temporal, vamos utilizar apenas dados de temperatura 
+palmerweather <- df %>% 
+  select(
+    Date, Temperature.Average..C., Temperature.High..C., Temperature.Low..C.
+  ) %>% 
+  mutate(
+    Date = ymd(Date)
+  ) %>% 
+  filter(
+    year(Date) >= 1997
+  ) %>% 
+  rename(
+    Temp_avg = Temperature.Average..C.,
+    Temp_min = Temperature.Low..C.,
+    Temp_max = Temperature.High..C.
+  ) %>% 
+  as_tsibble(index = Date)
+
+
+palmerweather %>% 
+  filter(is.na(Temp_avg))
+
+palmerweather %>% 
+  tsibble::fill_gaps() %>% 
+  model(
+    STL(Temp_avg)
+  )
+
+dados_imputar <- palmerweather %>% 
+  tsibble::fill_gaps() %>% 
+  ungroup() %>% 
+  as.data.frame()
+
+
+imp <- dados_imputar %>% 
+  mice(m = 1, method = "pmm", seed = 123)
+
+dados_imputar <- complete(imp)
+
+md.pattern(dados_imputar)
+stripplot(imp, Temp_avg~.imp)
+
+palmerweather_imp <- palmerweather %>% 
+  mutate(
+    Temp_avg = dados_imputar$Temp_avg,
+    Temp_min = dados_imputar$Temp_min,
+    Temp_max = dados_imputar$Temp_max
+  )
+
+palmerweather_imp %>% 
+  model(
+    STL(Temp_avg)
+  )
+
+
+library(leaflet)
+m = leaflet() %>% addTiles()
+m  # a map with the default OSM tile layer
+
+m = m %>% setView(-93.65, 42.0285, zoom = 17)
+m
+
+m %>% addPopups(-93.65, 42.0285, 'Here is the <b>Department of Statistics</b>, ISU')
